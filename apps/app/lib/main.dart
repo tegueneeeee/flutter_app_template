d0bba7da44265961cb62f1c605315d81ea03e433
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2025-04-01 16:51:09 +0900

    mod ios running

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/core/env/firebase_options.dart';
-import 'package:flutter_app/core/env/flavor.dart';
 import 'package:flutter_app/src/app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: firebaseOptionsWithFlavor(Flavor.prod));
 
   runApp(const ProviderScope(child: App()));
 }
