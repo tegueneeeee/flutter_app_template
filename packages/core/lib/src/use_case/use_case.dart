39bbf058c3d5db3aadbd546538aed84f5b3a0a3c
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2025-04-15 15:39:24 +0900

    Add generated plugin configuration and update analysis options
    - Introduced `.flutter-plugins` and `.flutter-plugins-dependencies` files to manage plugin dependencies for various platforms.
    - Updated `analysis_options.yaml` to ignore specific linting rules for better code flexibility.
    - Added a new `use_case.dart` file to define abstract use cases and parameter classes for improved code organization.
    - Updated `pubspec.yaml` to include a reference to shared dependencies for better modularity.

---
+++ packages/core/lib/src/use_case/use_case.dart
@@ -0,0 +1,28 @@
+import 'package:shared_dependencies/dependencies.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Type> call(Params params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class IDParams extends Equatable {
+  const IDParams({required this.id});
+
+  final String id;
+
+  @override
+  List<Object?> get props => [id];
+}
+
+class PaginationParams extends Equatable {
+  const PaginationParams({required this.page});
+
+  final int page;
+
+  @override
+  List<Object?> get props => [page];
+}
