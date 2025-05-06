440dfe82f1d013c5f6787d63206b2a0d1f88a5c0
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2025-05-07 02:00:40 +0900

    Add shared_preferences dependency and update app providers
    - Added shared_preferences dependency to pubspec.yaml.
    - Updated AppProviders to initialize SharedPreferences and include it in the override providers list.

--- apps/app/lib/app_providers.dart
+++ apps/app/lib/app_providers.dart
@@ -2,12 +2,14 @@ import 'package:core/core.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_config/config.dart';
 import 'package:shared_dependencies/dependencies.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final class AppProviders {
   static Future<List<Override>> initialize() async {
     final overrideProviders = <Override>[];
 
     final packageInfo = await PackageInfo.fromPlatform();
+    final sharedPreferences = await SharedPreferences.getInstance();
 
     final buildConfig = AppBuildConfig.withFlavorName(
       appFlavor: const String.fromEnvironment('flavor'),
@@ -23,6 +25,7 @@ final class AppProviders {
 
     overrideProviders.addAll([
       appBuildConfigStateProvider.overrideWithValue(buildConfig),
+      sharedPreferencesProvider.overrideWithValue(sharedPreferences),
     ]);
 
     return overrideProviders;
